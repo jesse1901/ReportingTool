5d8df082a94e70e55823aef243f5e18a966bb32b
jesse.schuetz <jesse.schuetz@desy.de>
2024-07-19 12:41:24 +0200

    SQL-Anbindung, Streamlit display

--- report.py
+++ report.py
@@ -96,18 +96,20 @@ if __name__ == "__main__":
 
     conn_streamlit = st.connection('reports_db', type='sql')
 
+    x = conn_streamlit.query("SELECT * FROM reportdata", ttl=600)
+    df = pd.DataFrame(x)
+    st.write(df)
+
     while True:
 
-        x = conn_streamlit.query("SELECT * FROM reportdata", ttl=600)
-        df = pd.DataFrame(x)
-        st.write(df)
         cur.execute("""
                     SELECT MAX(jobID) FROM reportdata
         """)
         x = cur.fetchall()
-        jobID = x[0]
+        jobID = x[0][0]
+        print(jobID)
         list_filter = []
-        for i in range(50):
+        for i in range(500):
             jobID += 1
             list_filter.append(jobID)
         db_filter = pyslurm.db.JobFilter(ids=list_filter)
