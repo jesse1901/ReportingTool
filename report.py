03f1b9dc9a6fcc95a9e9b06db7559ee96f079480
jesse.schuetz <jesse.schuetz@desy.de>
2024-07-24 10:45:51 +0200

    features

--- report.py
+++ report.py
@@ -5,6 +5,8 @@ import time
 from datetime import timedelta, datetime
 import sqlite3
 import plotly.express as px
+import requests
+import json
 
 
 def count_keys_under_steps(d):
@@ -26,6 +28,8 @@ def count_keys_under_steps(d):
 class GetStats:
     def __init__(self):
         # Initialize attributes for storing job statistics and calculations
+        self.gpu_eff = None
+        self.job_nodes = None
         self.latest_end = None
         self.jobs = None
         self.db_filter = None
@@ -33,12 +37,12 @@ class GetStats:
         self.cores_job = None
         self.job_id = 0
         self.cores = 0
-        self.used_time = ''
+        self.used_time = None
         self.job_eff = 0
         self.job_steps = {}
-        self.job_elapsed = ''
-        self.start = ''
-        self.end = ''
+        self.job_elapsed = None
+        self.start = None
+        self.end = None
         self.job_data = {}
         self.job_cpu = {}
         self.job_all = {}
@@ -50,6 +54,10 @@ class GetStats:
         self.latest_avg_eff = ''
         self.avg_eff = 0
         self.intervall = ''
+        self.nodelist = []
+        self.all_nodes = ['exflong03.desy.de', 'exflong04.desy.de', 'exflong05.desy.de', 'exflong06.desy.de', 'max-cfelg007.desy.de', 'max-cfelg007.desy.de', 'max-cmsg001.desy.de', 'max-cmsg002.desy.de', 'max-cmsg005.desy.de', 'max-cmsg006.desy.de', 'max-cmsg007.desy.de', 'max-cmsg008.desy.de', 'max-cmsg009.desy.de', 'max-cmsg010.desy.de', 'max-cssbg002.desy.de', 'max-cssbg002.desy.de', 'max-cssbg003.desy.de', 'max-cssbg003.desy.de', 'max-cssbg004.desy.de', 'max-cssbg004.desy.de', 'max-cssbg005.desy.de', 'max-cssbg005.desy.de', 'max-cssbg012.desy.de', 'max-cssbg012.desy.de', 'max-cssbg012.desy.de', 'max-cssbg012.desy.de', 'max-cssbg018.desy.de', 'max-cssbg018.desy.de', 'max-cssbg018.desy.de', 'max-cssbg018.desy.de', 'max-display001.desy.de', 'max-display001.desy.de', 'max-display001.desy.de', 'max-display001.desy.de', 'max-display002.desy.de', 'max-display002.desy.de', 'max-display002.desy.de', 'max-display002.desy.de', 'max-display003.desy.de', 'max-display003.desy.de', 'max-display003.desy.de', 'max-display003.desy.de', 'max-display004.desy.de', 'max-display004.desy.de', 'max-display004.desy.de', 'max-display004.desy.de', 'max-display005.desy.de', 'max-display005.desy.de', 'max-display005.desy.de', 'max-display005.desy.de', 'max-display007.desy.de', 'max-display007.desy.de', 'max-display008.desy.de', 'max-display008.desy.de', 'max-display009.desy.de', 'max-display009.desy.de', 'max-display010.desy.de', 'max-display010.desy.de', 'max-exfl-display001.desy.de', 'max-exfl-display001.desy.de', 'max-exfl-display002.desy.de', 'max-exfl-display002.desy.de', 'max-exfl-display003.desy.de', 'max-exfl-display003.desy.de', 'max-exfl-display004.desy.de', 'max-exfl-display004.desy.de', 'max-exflg005.desy.de', 'max-exflg005.desy.de', 'max-exflg006.desy.de', 'max-exflg007.desy.de', 'max-exflg009.desy.de', 'max-exflg010.desy.de', 'max-exflg011.desy.de', 'max-exflg012.desy.de', 'max-exflg013.desy.de', 'max-exflg014.desy.de', 'max-exflg015.desy.de', 'max-exflg016.desy.de', 'max-exflg017.desy.de', 'max-exflg018.desy.de', 'max-exflg019.desy.de', 'max-exflg020.desy.de', 'max-exflg021.desy.de', 'max-exflg022.desy.de', 'max-exflg023.desy.de', 'max-exflg024.desy.de', 'max-exflg025.desy.de', 'max-exflg026.desy.de', 'max-exflg027.desy.de', 'max-exflg028.desy.de', 'max-exflg028.desy.de', 'max-exflg029.desy.de',
+                          'max-exflg029.desy.de', 'max-exflg030.desy.de', 'max-exflg030.desy.de', 'max-exflg031.desy.de', 'max-exflg031.desy.de', 'max-exflg032.desy.de', 'max-exflg033.desy.de', 'max-exflg034.desy.de', 'max-exflg035.desy.de', 'max-fs-display001.desy.de', 'max-fs-display001.desy.de', 'max-fs-display002.desy.de', 'max-fs-display002.desy.de', 'max-fs-display003.desy.de', 'max-fs-display003.desy.de', 'max-fs-display004.desy.de', 'max-fs-display004.desy.de', 'max-fs-display005.desy.de', 'max-fs-display005.desy.de', 'max-fs-display006.desy.de', 'max-fs-display006.desy.de', 'max-hzgg001.desy.de', 'max-hzgg001.desy.de', 'max-hzgg003.desy.de', 'max-hzgg003.desy.de', 'max-hzgg004.desy.de', 'max-hzgg004.desy.de', 'max-hzgg007.desy.de', 'max-hzgg007.desy.de', 'max-hzgg008.desy.de', 'max-hzgg008.desy.de', 'max-hzgg009.desy.de', 'max-hzgg009.desy.de', 'max-hzgg010.desy.de', 'max-hzgg010.desy.de', 'max-nova001.desy.de', 'max-nova001.desy.de', 'max-nova002.desy.de', 'max-nova002.desy.de', 'max-p3ag004.desy.de', 'max-p3ag005.desy.de', 'max-p3ag007.desy.de', 'max-p3ag008.desy.de', 'max-p3ag010.desy.de', 'max-p3ag011.desy.de', 'max-p3ag011.desy.de', 'max-p3ag011.desy.de', 'max-p3ag011.desy.de', 'max-p3ag012.desy.de', 'max-p3ag014.desy.de', 'max-p3ag015.desy.de', 'max-p3ag016.desy.de', 'max-p3ag017.desy.de', 'max-p3ag018.desy.de', 'max-p3ag019.desy.de', 'max-p3ag020.desy.de', 'max-p3ag021.desy.de', 'max-p3ag022.desy.de', 'max-p3ag023.desy.de', 'max-p3ag024.desy.de', 'max-p3ag025.desy.de', 'max-p3ag026.desy.de', 'max-p3ag027.desy.de', 'max-p3ag028.desy.de', 'max-p3ag029.desy.de', 'max-p3ag030.desy.de', 'max-p3ag031.desy.de', 'max-uhhg001.desy.de', 'max-uhhg001.desy.de', 'max-uhhg001.desy.de', 'max-uhhg001.desy.de', 'max-uhhg002.desy.de', 'max-uhhg002.desy.de', 'max-uhhg002.desy.de', 'max-uhhg002.desy.de', 'max-uhhg003.desy.de', 'max-uhhg003.desy.de', 'max-uhhg003.desy.de', 'max-uhhg003.desy.de', 'max-uhhg004.desy.de', 'max-uhhg004.desy.de', 'max-uhhg004.desy.de', 'max-uhhg004.desy.de', 'max-uhhg005.desy.de', 'max-uhhg005.desy.de', 'max-uhhg005.desy.de', 'max-uhhg005.desy.de', 'max-uhhg006.desy.de', 'max-uhhg006.desy.de', 'max-uhhg006.desy.de', 'max-uhhg006.desy.de', 'max-uhhg007.desy.de', 'max-uhhg007.desy.de', 'max-uhhg007.desy.de', 'max-uhhg007.desy.de', 'max-uhhg008.desy.de', 'max-uhhg008.desy.de',
+                          'max-uhhg008.desy.de', 'max-uhhg008.desy.de', 'max-uhhg009.desy.de', 'max-uhhg009.desy.de', 'max-uhhg009.desy.de', 'max-uhhg009.desy.de', 'max-uhhg010.desy.de', 'max-uhhg010.desy.de', 'max-uhhg010.desy.de', 'max-uhhg010.desy.de', 'max-uhhg011.desy.de', 'max-uhhg011.desy.de', 'max-uhhg011.desy.de', 'max-uhhg011.desy.de', 'max-uhhg012.desy.de', 'max-uhhg012.desy.de', 'max-uhhg012.desy.de', 'max-uhhg012.desy.de', 'max-uhhg013.desy.de', 'max-uhhg013.desy.de', 'max-uhhg013.desy.de', 'max-uhhg013.desy.de', 'max-wng002.desy.de', 'max-wng002.desy.de', 'max-wng004.desy.de', 'max-wng005.desy.de', 'max-wng006.desy.de', 'max-wng007.desy.de', 'max-wng008.desy.de', 'max-wng008.desy.de', 'max-wng009.desy.de', 'max-wng009.desy.de', 'max-wng010.desy.de', 'max-wng012.desy.de', 'max-wng013.desy.de', 'max-wng014.desy.de', 'max-wng015.desy.de', 'max-wng016.desy.de', 'max-wng017.desy.de', 'max-wng018.desy.de', 'max-wng019.desy.de', 'max-wng020.desy.de', 'max-wng020.desy.de', 'max-wng021.desy.de', 'max-wng021.desy.de', 'max-wng022.desy.de', 'max-wng023.desy.de', 'max-wng037.desy.de', 'max-wng037.desy.de', 'max-wng037.desy.de', 'max-wng037.desy.de', 'max-wng038.desy.de', 'max-wng038.desy.de', 'max-wng038.desy.de', 'max-wng038.desy.de', 'max-wng039.desy.de', 'max-wng039.desy.de', 'max-wng039.desy.de', 'max-wng039.desy.de', 'max-wng040.desy.de', 'max-wng040.desy.de', 'max-wng040.desy.de', 'max-wng040.desy.de', 'max-wng041.desy.de', 'max-wng041.desy.de', 'max-wng041.desy.de', 'max-wng041.desy.de', 'max-wng042.desy.de', 'max-wng042.desy.de', 'max-wng042.desy.de', 'max-wng042.desy.de', 'max-wng061.desy.de', 'max-wng061.desy.de', 'max-wng061.desy.de', 'max-wng061.desy.de', 'max-wng062.desy.de', 'max-wng062.desy.de', 'max-wng062.desy.de', 'max-wng062.desy.de', 'max-wng063.desy.de', 'max-wng064.desy.de']
 
     def job_stats(self, job_id: int) -> None:
         """
@@ -62,6 +70,7 @@ class GetStats:
         self.job_elapsed_s = self.job_data.elapsed_time
         self.cores = self.job_data.cpus
         self.job_steps = count_keys_under_steps(self.job_all)
+        self.nodelist = self.job_data.nodelist
 
         # Calculate total CPU time used for job steps
         for step in self.job_steps:
@@ -90,6 +99,54 @@ class GetStats:
             self.job_eff = round((self.total_cpu_sum / (self.cores * self.job_elapsed_s)) * 100, 1)
         else:
             self.job_eff = 0
+
+    def get_jobs_calculate_insert_data(self, cur) -> None:
+        """
+        Fetches jobs, calculates their statistics, and inserts them into the database.
+        """
+        # Retrieve the highest jobID currently in the reportdata table
+        cur.execute("SELECT MAX(end) FROM reportdata")
+        self.latest_end = str(cur.fetchone()[0] or 0)
+
+        # Create a list of job IDs to filter and load jobs
+        self.list_filter = round(time.time())
+        # [self.jobID_count + i + 1 for i in range(1000)]
+        self.db_filter = pyslurm.db.JobFilter(end_time=self.list_filter)
+        self.jobs = pyslurm.db.Jobs.load(self.db_filter)
+
+        # Process each job
+        for job_id in self.jobs.keys():
+            try:
+                stats = GetStats()
+                stats.job_stats(job_id)
+                set_nodes = set(self.all_nodes)
+                self.job_nodes = [node for node in self.nodelist if node in set_nodes]
+#                if self.end is not None:
+#                    end_time = datetime.fromtimestamp()
+#                    end_time = end_time.isoformat('T', 'auto')
+                if self.job_nodes and self.end is not None:
+                    GetStats.get_gpu_data()
+                    try:
+                        if self.end is not None:
+                            print(f'execute query cause: {self.end}  jobID: {job_id}')
+                            data = stats.to_dict()
+                            # Insert job statistics into reportdata table, avoiding conflicts on unique jobID
+                            cur.execute("""
+                                INSERT INTO reportdata (
+                                    jobID, username, account, efficiency, used_time, booked_time,
+                                    state, gpu_nodes, cores, start, end
+                                ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?) ON CONFLICT(jobID) DO NOTHING
+                            """, (
+                                data['job_id'], data['user'], data['account'], data['efficiency'],
+                                data['used'], data['booked'], data['state'], data['gpu_nodes'], data['cores'],
+                                data['start'], data['end']
+                            ))
+                            cur.connection.commit()
+                    except Exception as err:
+                        print(f'Error endtime, job {job_id}:{err}')
+            except Exception as e:
+                # Print an error message if job processing fails
+                print(f"Error processing job {job_id}: {e}")
 
     def calculate_avg_eff(self, cur) -> None:
         """
@@ -138,53 +195,25 @@ class GetStats:
         time.sleep(2)
         return
 
-    def get_jobs_calculate_insert_data(self, cur) -> None:
-        """
-        Fetches jobs, calculates their statistics, and inserts them into the database.
-        """
-        # Retrieve the highest jobID currently in the reportdata table
-        cur.execute("SELECT MAX(end) FROM reportdata")
-        self.latest_end = str(cur.fetchone()[0] or 0)
+    def get_gpu_data(self):
+        prometheus_url = 'https://max-infra008.desy.de:9090/api/v1/query_range'
+        join_nodes = '|'.join(self.job_nodes)
+        params = {
+            'query': f'nvidia_smi_utilization_gpu_ratio{{instance="{join_nodes}"}}',
+            'start': self.start,
+            'end': self.end,
+            'step': '1m'
+        }
+        response = requests.get(prometheus_url, params=params)
+        self.gpu_eff = response.json()['gpu_usage']
 
-        # Create a list of job IDs to filter and load jobs
-        self.list_filter = round(time.time())
-# [self.jobID_count + i + 1 for i in range(1000)]
-        self.db_filter = pyslurm.db.JobFilter(end_time=self.list_filter)
-        self.jobs = pyslurm.db.Jobs.load(self.db_filter)
+        print(response.json())
 
-        # Process each job
-        for job_id in self.jobs.keys():
-            try:
-                stats = GetStats()
-                stats.job_stats(job_id)
-                if stats.job_data.end_time is not None:
-                    end_time = datetime.fromtimestamp(stats.job_data.end_time)
-                    end_time = end_time.isoformat('T', 'auto')
-                    try:
-                        if end_time is not None and end_time > self.latest_end:
-                            print(f'execute query cause: {end_time} > {self.latest_end} jobID: {job_id}')
-                            data = stats.to_dict()
-                            # Insert job statistics into reportdata table, avoiding conflicts on unique jobID
-                            cur.execute("""
-                                INSERT INTO reportdata (
-                                    jobID, username, account, efficiency, used_time, booked_time,
-                                    state, cores, start, end
-                                ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?) ON CONFLICT(jobID) DO NOTHING
-                            """, (
-                                data['job_id'], data['user'], data['account'], data['efficiency'],
-                                data['used'], data['booked'], data['state'], data['cores'],
-                                data['start'], data['end']
-                            ))
-                            cur.connection.commit()
-                    except Exception as err:
-                        print(f'Error endtime, job {job_id}:{err}')
-            except Exception as e:
-                # Print an error message if job processing fails
-                print(f"Error processing job {job_id}: {e}")
+
 
     def to_dict(self) -> dict:
         """
-        Converts job statistics to a dictionary format.
+        Converts job statistics to a dictionary format
         """
         return {
             "job_id": self.job_id,
@@ -194,6 +223,8 @@ class GetStats:
             "used": self.used_time,
             "booked": self.job_elapsed,
             "state": self.job_data.state,
+            "gpu_nodes": self.job_nodes,
+            "gpu_efficiency": self.
             "cores": self.cores,
             "start": self.start,
             "end": self.end,
@@ -248,7 +279,7 @@ if __name__ == "__main__":
     cur = con.cursor()
     # Create figures and display them
     create = CreateFigures(con)
-    create.frame_user_all()
+#    create.frame_user_all()
     create.frame_group_by_user()
     create.chart_cpu_utilization()
     create.scatter_chart_data()
@@ -257,12 +288,19 @@ if __name__ == "__main__":
     while True:
         x = 29
         get = GetStats()
-        if x == 30:
-            print('XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX')
-            get.calculate_avg_eff(cur)
-            x = 0
         get.get_jobs_calculate_insert_data(cur)
+        if x == 30:
+#            print('XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX')
+#            get.calculate_avg_eff(cur)
+            x = 0
         x += 1
+
+
+
+
+
+
+
 
 
 
@@ -276,6 +314,8 @@ if __name__ == "__main__":
     #         used_time TEXT,
     #         booked_time TEXT,
     #         state TEXT,
+    #         gpu_nodes TEXT,
+    #         gpu_eff REAL,
     #         cores INT,
     #         start TEXT,
     #         end TEXT
