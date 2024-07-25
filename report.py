cac85f5a92f36b9ee19ae9087ea5e09e52091859
jesse.schuetz <jesse.schuetz@desy.de>
2024-07-25 14:10:16 +0200

    features

--- report.py
+++ report.py
@@ -401,7 +401,7 @@ class CreateFigures:
                     FROM reportdata
                     ORDER BY job_cpu_time_s ASC;""", self.con)
 
-        fig = px.scatter(df, x="job_cpu_time", y="cpu_efficiency", color= "gpu_efficiency" if "gpu_efficiency" else "cpu_efficiency", size_max=1,
+        fig = px.scatter(df, x="job_cpu_time_s", y="cpu_efficiency", color= "gpu_efficiency" if "gpu_efficiency" else "cpu_efficiency", size_max=1,
                          hover_data=["jobID", "username", "lost_cpu_time", "lost_gpu_time", "real_time", "cores"])
         st.plotly_chart(fig, theme=None)
 
