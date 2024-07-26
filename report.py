88421bd27d97a7577885832a00c54209442aab61
jesse.schuetz <jesse.schuetz@desy.de>
2024-07-26 12:02:27 +0200

    features

--- report.py
+++ report.py
@@ -387,13 +387,13 @@ class CreateFigures:
         df = df.dropna(subset=['job_cpu_time_s'])
         df['job_cpu_time_s'] = df['job_cpu_time_s'].astype(int)
         df['job_cpu_time_s'] = df['job_cpu_time_s'].apply(seconds_to_timestring)
-        fig = px.scatter(df, x="job_cpu_time_s", y="cpu_efficiency", color= "gpu_efficiency", color_continuous_scale="sunsetdark", size_max=1,
+        fig = px.scatter(df, x="job_cpu_time_s", y="cpu_efficiency", color= "gpu_efficiency", color_continuous_scale="plotly3", size_max=1,
                          hover_data=["jobID", "username", "lost_cpu_time", "lost_gpu_time", "real_time", "cores", "state"],
                          labels={
                              "job_cpu_time_s": "real_job_time"
                          }
                          )
-        fig.update_traces(marker=dict(size=3))
+        fig.update_traces(marker=dict(size=5))
         st.plotly_chart(fig, theme=None)
 
 
