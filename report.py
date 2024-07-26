27b763cd50c7ca374e653fb28cd211b5958dba3b
jesse.schuetz <jesse.schuetz@desy.de>
2024-07-26 12:12:15 +0200

    features

--- report.py
+++ report.py
@@ -379,22 +379,32 @@ class CreateFigures:
 
     def scatter_chart_data_cpu_gpu_eff(self):
         df = pd.read_sql_query("""
-            SELECT jobID, username, gpu_efficiency, IFNULL(gpu_efficiency, '0' ) AS gpu_efficiency, cpu_efficiency, lost_cpu_time, lost_gpu_time, job_cpu_time_s, real_time, cores, state
-                    FROM reportdata
-                    ORDER BY job_cpu_time_s ASC;""", self.con)
+            SELECT jobID, username, IFNULL(gpu_efficiency, 0) AS gpu_efficiency, 
+                   cpu_efficiency, lost_cpu_time, lost_gpu_time, job_cpu_time_s, real_time, cores, state
+            FROM reportdata
+            ORDER BY job_cpu_time_s ASC;""", self.con)
 
         df['job_cpu_time_s'] = pd.to_numeric(df['job_cpu_time_s'], errors='coerce')
         df = df.dropna(subset=['job_cpu_time_s'])
         df['job_cpu_time_s'] = df['job_cpu_time_s'].astype(int)
         df['job_cpu_time_s'] = df['job_cpu_time_s'].apply(seconds_to_timestring)
-        fig = px.scatter(df, x="job_cpu_time_s", y="cpu_efficiency", color="gpu_efficiency", color_continuous_scale="plotly3", size_max=1,
-                         hover_data=["jobID", "username", "lost_cpu_time", "lost_gpu_time", "real_time", "cores", "state"],
-                         labels={
-                             "job_cpu_time_s": "real_job_time"
-                         }
-                         )
+
+        fig = px.scatter(
+            df,
+            x="job_cpu_time_s",
+            y="cpu_efficiency",
+            color="gpu_efficiency",
+            color_continuous_scale="plotly3",
+            size_max=1,
+            hover_data=["jobID", "username", "lost_cpu_time", "lost_gpu_time", "real_time", "cores", "state"],
+            labels={"job_cpu_time_s": "real_job_time"}
+        )
+
         fig.update_traces(marker=dict(size=5))
         st.plotly_chart(fig, theme=None)
+
+    # Beispiel wie die Funktion aufgerufen werden könnte
+    # scatter_chart_data_cpu_gpu_eff()
 
 
 
