6ea7ca7fcdf5cfe0c13eb9bda780d0ae8f07175c
jesse.schuetz <jesse.schuetz@desy.de>
2024-07-26 12:07:35 +0200

    features

--- report.py
+++ report.py
@@ -379,7 +379,7 @@ class CreateFigures:
 
     def scatter_chart_data_cpu_gpu_eff(self):
         df = pd.read_sql_query("""
-            SELECT jobID, username, gpu_efficiency, IFNULL(gpu_efficiency,0), cpu_efficiency, lost_cpu_time, lost_gpu_time, job_cpu_time_s, real_time, cores, state
+            SELECT jobID, username, gpu_efficiency, IFNULL(gpu_efficiency, '0' ), cpu_efficiency, lost_cpu_time, lost_gpu_time, job_cpu_time_s, real_time, cores, state
                     FROM reportdata
                     ORDER BY job_cpu_time_s ASC;""", self.con)
 
