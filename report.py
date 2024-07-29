630ba26be772d17daad08bc8623868b689de04cc
jesse.schuetz <jesse.schuetz@desy.de>
2024-07-29 09:57:42 +0200

    features

--- report.py
+++ report.py
@@ -247,7 +247,7 @@ class GetStats:
         return
 
     def get_gpu_data(self):
-        step = '1'
+        step = 1
         prometheus_url = 'http://max-infra008.desy.de:9090/api/v1/query_range'
         params = {
             'query': f'nvidia_smi_utilization_gpu_ratio{{instance="{self.join_nodes}"}}',
