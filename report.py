f13ca4e1e15df93edfdc0ba398182ff2a84d19af
jesse.schuetz <jesse.schuetz@desy.de>
2024-07-30 09:32:22 +0200

    features

--- report.py
+++ report.py
@@ -297,8 +297,8 @@ class GetStats:
                 int_values = [float(value[1]) for value in values]
                 self.gpu_eff = (sum(int_values) / len(int_values)) if int_values else 0
                 if self.job_gpu_nodes is not None and self.job_elapsed_s is not None:
-                    self.lost_gpu_time_seconds = len(self.job_gpu_nodes) * self.job_elapsed_s * (1 - self.gpu_eff)
-                    self.lost_gpu_time_sec = round(self.lost_gpu_time_seconds)
+                    self.lost_gpu_time_seconds = round(len(self.job_gpu_nodes) * self.job_elapsed_s * (1 - self.gpu_eff))
+                    self.lost_gpu_time_sec = self.lost_gpu_time_seconds
                     print(f'round CPU : {self.lost_gpu_time_sec}')
                     self.lost_gpu_time = str(timedelta(seconds=self.lost_gpu_time_sec))
                     #print(f"gpu-usage: {self.gpu_eff}"))
