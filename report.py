787574c78dab2d5abe641ef5d013e1211e532bdf
jesse.schuetz <jesse.schuetz@desy.de>
2024-07-30 15:06:42 +0200

    features

--- report.py
+++ report.py
@@ -497,6 +497,7 @@ class CreateFigures:
         # Filter dataframe based on the checkbox
         if hide_gpu_none:
             df = df[df['gpu_efficiency'].isna()]
+            return
         # Create scatter plot
         fig = px.scatter(
             df,
