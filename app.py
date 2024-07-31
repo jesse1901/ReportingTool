22f579b8dadcef44d4d1ffa4f2a28dfbf793ae4d
jesse.schuetz <jesse.schuetz@desy.de>
2024-07-31 15:24:26 +0200

    features

--- app.py
+++ app.py
@@ -283,7 +283,7 @@ class CreateFigures:
         # Filter dataframe based on the checkbox
         row_var = ['gpu_efficiency']
         if hide_gpu_none:
-            df = df[~df[row_var].isnull()]
+            df = df[df[row_var].isna()]
 
             #df = df.dropna(subset=['gpu_efficiency'])
         # Create scatter plot
