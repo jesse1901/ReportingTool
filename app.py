b809504d5aaf4b19ef7f8c9467d8c7fb8a390925
jesse.schuetz <jesse.schuetz@desy.de>
2024-08-02 10:33:46 +0200

    features

--- app.py
+++ app.py
@@ -528,9 +528,8 @@ class CreateFigures:
                """, self.con)
 
         # Check if there are enough unique values in 'cpu_efficiency' to calculate percentiles
-        if df['cpu_efficiency'].nunique() < 10:
-            st.error("Nicht genügend einzigartige cpu_efficiency-Werte, um Perzentile zu berechnen.")
-            return
+        df = df[df['cpu_efficiency'] != 0]
+
 
         # Calculate percentiles for 'cpu_efficiency'
         df['efficiency_percentile'] = pd.qcut(df['cpu_efficiency'], 10, labels=False, duplicates='drop')
