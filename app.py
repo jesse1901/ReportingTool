ecee715d8c11deb6323b8154b0d8660b73c2b726
jesse.schuetz <jesse.schuetz@desy.de>
2024-08-06 15:26:38 +0200

    features

--- app.py
+++ app.py
@@ -599,7 +599,7 @@ if __name__ == "__main__":
         client_id="maxwell-reporting-oidc",
         init_options={
             "pkceMethod": "S256",
-            "checkLoginIframe": False
+            "checkLoginIframe": True
         }
     )
     if keycloak.authenticated:
