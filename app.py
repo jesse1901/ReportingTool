e826aecf5b45eec266cf47409a30912f2be702b8
jesse.schuetz <jesse.schuetz@desy.de>
2024-08-08 14:06:44 +0200

    features

--- app.py
+++ app.py
@@ -208,20 +208,15 @@ class CreateFigures:
         df = df.sort_values(by='total_lost_cpu_time', ascending=False).head(display_user)
 
         scale_efficiency = st.checkbox("Hyperthreading Aus")
+        if scale_efficiency:
+            df['total_job_time'] = np.where(df['avg_cpu_efficiency'] != 100, df['total_lost_cpu_time'] / ((100 - df['avg_cpu_efficiency']) / 100), np.nan)
+            df['cpu_efficiency'] = df.apply(
+                lambda row: min(row['avg_cpu_efficiency'] * 2, 100) if row['avg_cpu_efficiency'] <= 100 else row['avg_cpu_efficiency'], axis=1)
 
-        df['total_job_time'] = np.where(df['avg_cpu_efficiency'] != 100,
-                                        df['total_lost_cpu_time'] / ((100 - df['avg_cpu_efficiency']) / 100),
-                                        np.nan)
-        df['cpu_efficiency'] = df.apply(
-            lambda row: min(row['avg_cpu_efficiency'] * 2, 100) if row['avg_cpu_efficiency'] <= 100 else row[
-                'avg_cpu_efficiency'], axis=1)
-
-        # Handle NaN in cpu_efficiency and total_job_time
-        df['cpu_efficiency'] = df['cpu_efficiency'].fillna(0)
-        df['total_lost_cpu_time'] = np.where(df['cpu_efficiency'] != 0,
-                                             df['total_job_time'] / df['cpu_efficiency'],
-                                             np.nan)
-        df['total_lost_cpu_time'] = df['total_lost_cpu_time'].fillna(0)
+            # Handle NaN in cpu_efficiency and total_job_time
+            df['cpu_efficiency'] = df['cpu_efficiency'].fillna(0)
+            df['total_lost_cpu_time'] = np.where(df['cpu_efficiency'] != 0, df['total_job_time'] / df['cpu_efficiency'], np.nan)
+            df['total_lost_cpu_time'] = df['total_lost_cpu_time'].fillna(0)
 
         # Define constant tick values for the y-axis (vertical chart)
         max_lost_time = df['total_lost_cpu_time'].max()
