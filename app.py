035bd2b7a6498e3c274eecaca829a5881b36f14a
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 <jesse.schuetz@desy.de>
2024-10-21 12:37:39 +0200

    custom select on first page

--- app.py
+++ app.py
@@ -129,7 +129,7 @@ class CreateFigures:
 
             # Zeige die SQL-Abfrage in Streamlit an
             st.write("Die erstellte SQL-Abfrage lautet:")
-            pd.read_sql_query(sql_query, self.con)
+            df = pd.read_sql_query(sql_query, self.con)
         # df = pd.read_sql_query("""
         #     SELECT jobID, username, account, cpu_efficiency, lost_cpu_time, lost_cpu_time_sec, gpu_efficiency, lost_gpu_time,
         #     lost_gpu_time_sec, real_time, job_cpu_time, real_time_sec, state, cores, gpu_nodes, start, end, job_name, partition
