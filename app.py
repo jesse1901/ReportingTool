e3fc7450e68425b07b73c77288d40488cf6c28f2
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 <jesse.schuetz@desy.de>
2024-10-21 12:24:01 +0200

    custom query

--- app.py
+++ app.py
@@ -111,15 +111,23 @@ class CreateFigures:
         Displays all job data.py from the reportdata table in the Streamlit app.
         """
         st.write('All Data')
-        sql_selector = ["jobID", "username", "account", "cpu_efficiency", "lost_cpu_time", "lost_cpu_time_sec", "gpu_efficiency", "lost_gpu_time",
+        sql_select = ["jobID", "username", "account", "cpu_efficiency", "lost_cpu_time", "lost_cpu_time_sec", "gpu_efficiency", "lost_gpu_time",
             "lost_gpu_time_sec", "real_time", "job_cpu_time", "real_time_sec", "state", "cores", "gpu_nodes", "start", "end", "job_name", "partition" ]
         select = st.multiselect('Select Data', sql_selector)
 
-        df = pd.read_sql_query("""
-            SELECT jobID, username, account, cpu_efficiency, lost_cpu_time, lost_cpu_time_sec, gpu_efficiency, lost_gpu_time, 
-            lost_gpu_time_sec, real_time, job_cpu_time, real_time_sec, state, cores, gpu_nodes, start, end, job_name, partition 
-            FROM reportdata
-            """, self.con)
+        st.write('All Data')
+        sql_where = ["jobID", "username", "account", "cpu_efficiency", "lost_cpu_time", "lost_cpu_time_sec",
+                        "gpu_efficiency", "lost_gpu_time",
+                        "lost_gpu_time_sec", "real_time", "job_cpu_time", "real_time_sec", "state", "cores",
+                        "gpu_nodes", "start", "end", "job_name", "partition"]
+        select = st.multiselect('Select Data', sql_select)
+        sql_where_condition = st.text_input
+        df = pd.read_sql_query(f'SELECT {sql_select} FROM reportdata WHERE {sql_where_condition}', self.con)
+        # df = pd.read_sql_query("""
+        #     SELECT jobID, username, account, cpu_efficiency, lost_cpu_time, lost_cpu_time_sec, gpu_efficiency, lost_gpu_time,
+        #     lost_gpu_time_sec, real_time, job_cpu_time, real_time_sec, state, cores, gpu_nodes, start, end, job_name, partition
+        #     FROM reportdata
+        #     """, self.con)
         st.write(df)
 
     def frame_group_by_user(self) -> None:
