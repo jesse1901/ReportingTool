27feb2e823158e43268351174ebb3f73a19117fc
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 <jesse.schuetz@desy.de>
2024-10-22 10:38:11 +0200

    fix query

--- app.py
+++ app.py
@@ -113,7 +113,7 @@ class CreateFigures:
         """
         Displays all job data.py from the reportdata table in the Streamlit app.
         """
-        
+
         st.write('All Data')
 
         # List of available columns for     selection
@@ -263,6 +263,7 @@ class CreateFigures:
         # Define constant tick values for the y-axis (vertical chart)
         max_lost_time = df['total_lost_cpu_time'].max()
         tick_vals = np.linspace(0, max_lost_time, num=10)
+        tick_vals = [val for val in tick_vals if not np.isnan(val)]
         tick_text = [seconds_to_timestring(int(val)) for val in tick_vals]
 
         # Plot vertical bar chart using Plotly
