a89900a11d23d95660410526f033e1ec0616216f
jesse1901 <jesseschuetz@gmx.de>
2024-11-06 12:26:29 +0100

    add on_select

--- figures.py
+++ figures.py
@@ -158,7 +158,8 @@ class CreateFigures:
     hide_index=True)
             row = event.selection.rows
             filtered_df = df.iloc[row]
-            if  len(filtered_df) > 0:
+            st.write(len(filtered_df))
+            if len(filtered_df) > 0:
                 CreateFigures.get_job_script(_self, jobid=filtered_df.jobID.iloc[0])
         else:
             st.dataframe(df)
