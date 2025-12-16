9de6b043af0ad9780ddf2f53d48eda9f6aa6f976
jesse1901 <jesseschuetz@gmx.de>
2025-12-16 13:40:33 +0100

    docs

--- src/documentation.py
+++ src/documentation.py
@@ -31,7 +31,7 @@ class Documentation:
             st.markdown("""
             * **CPU_hours_used:** The actual CPU seconds used (sum over all cores).
             
-            * **CPU_hours_lost / Lost_CPU_Time:** CPU-Time that is not effectively used.<br>Formula: CPU_hours - CPU_hours_used
+            * **CPU_hours_lost / Lost_CPU_Time:** CPU-Time that is not effectively used by the job.<br>Formula: CPU_hours - CPU_hours_used
 
             * **CPUEff:** CPU Efficiency. Indicates how well the reserved CPUs were utilized.<br>Formula: TotalCPU / CPUTime
             """, unsafe_allow_html=True)
