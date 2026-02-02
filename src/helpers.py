92e67cc1893a0101d4b760a12f6721cb3956f7ce
jesse1901 <jesseschuetz@gmx.de>
2026-02-02 11:46:39 +0100

    db connection fix

--- src/helpers.py
+++ src/helpers.py
@@ -6,6 +6,18 @@ from datetime import timedelta
 import os
 import duckdb
 class helpers:
+
+    def get_db_mtime(db_path):
+        try:
+            return os.path.getmtime(db_path)
+        except OSError:
+            return 0
+
+    def get_connection(db_path):
+        # read_only=True ist wichtig, damit keine .wal Files blockieren
+        return duckdb.connect(db_path, read_only=True)
+
+
     def timestring_to_seconds(timestring):
         if pd.isna(timestring) or timestring == '0' or timestring == 0 or timestring.strip() == '':
             return 0
@@ -158,13 +170,3 @@ class helpers:
         return query, params
 
 
-
-    def get_db_mtime(db_path):
-        try:
-            return os.path.getmtime(db_path)
-        except OSError:
-            return 0
-
-    def get_connection(db_path):
-        # read_only=True ist wichtig, damit keine .wal Files blockieren
-        return duckdb.connect(db_path, read_only=True)
