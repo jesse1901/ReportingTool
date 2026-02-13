e1fe9bbeb9dcca2b736fbeed0a0e69b2b57d33e5
jesse1901 <jesseschuetz@gmx.de>
2026-02-13 11:22:13 +0100

    add st.toggle barchart grouped/stacked

--- src/charts/gpu_bar.py
+++ src/charts/gpu_bar.py
@@ -11,7 +11,7 @@ class GpuBarCharts:
         self.db_path = db_path
         
     @st.cache_data(ttl=600, show_spinner=False) 
-    def bar_chart_by_user_gpu(_self, start_date, end_date, current_user, user_role, number=None, partition_selector=None, allowed_groups=None, bar_mode="Stacked", scale_type="Absolute", sort_by="Total GPU", sort_by_percentage=False, min_total_days=0) -> None:
+    def bar_chart_by_user_gpu(_self, start_date, end_date, current_user, user_role, number=None, partition_selector=None, allowed_groups=None, scale_type="Absolute", sort_by="Total GPU", sort_by_percentage=False, min_total_days=0) -> None:
         st.markdown('Total GPU-Time per User', help='Partition "jhub" and Interactive Jobs are excluded. Only jobs with allocated GPUs are considered.')
 
         params = [start_date, end_date]
